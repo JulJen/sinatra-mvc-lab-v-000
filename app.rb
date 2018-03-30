668dc7c43761d22dfcf05d8381111764c7175664
Jul Jen <juliajen.prog@gmail.com>
2018-03-30 23:41:48 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -10,16 +10,7 @@ class App < Sinatra::Base
   post '/piglatinize' do
     go = PigLatinizer.new
 
-    if params[:user_phrase].split(" ").count == 1
-      @piglatinized_string = go.piglatinize(params[:user_phrase])
-    else
-      @piglatinized_string = go.pig_latin(params[:user_phrase])
-    end
+    @piglatinize = go.piglatinize(params[:user_phrase])
     erb :results
   end
 end
-
-#     @piglatinize = go.translate(params[:user_phrase])
-#     erb :results
-#   end
-# end
