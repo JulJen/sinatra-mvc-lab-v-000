8c03dc3ddf4382907ca9d1f1b2ea1ecbd6d9e188
Jul Jen <juliajen.prog@gmail.com>
2018-03-31 00:22:12 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,10 +1,8 @@
 require 'pry'
 
 class PigLatinizer
-  # attr_reader :text
 
   def piglatinize(str)
-# binding.pry
     if str.split(" ").count == 1
       translate_piglatinize(str)
     else
@@ -16,14 +14,13 @@ class PigLatinizer
     a = ('a'..'z').to_a
     v = %w[a e i o u A E I O U]
     c = a - v
-# binding.pry
+
     if c.include?(word[0].downcase) && c.include?(word[1]) && c.include?(word[2])
       word[3..-1] + word[0..2] + 'ay'
     elsif c.include?(word[0].downcase) && c.include?(word[1])
       word[2..-1] + word[0..1] + 'ay'
     elsif c.include?(word[0].downcase)
       word[1..-1] + word[0] + 'ay'
-# binding.pry
     else
       word + 'way'
     end
@@ -31,8 +28,7 @@ class PigLatinizer
 
 
   def pig_latin(str)
-# binding.pry
     str.split.map {|word| translate_piglatinize(word)}.join(" ")
   end
-  
+
 end
